c7557d3f1f1546a7c94da4755f07bf8ec945945c
Hemraj Persaud <persaud_hemraj@yahoo.com>
2025-07-14 22:28:56 -0400

    Create Login Page and Register page

---
+++ MovieDirectoryDotNetCoreMVC/Controllers/LogInController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieDirectoryDotNetCoreMVC.Models;
+
+namespace MovieDirectoryDotNetCoreMVC.Controllers
+{
+    public class LogInController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("LogIn");
+        }
+
+        public IActionResult LogIn()
+        {
+            return View("LogIn");
+        }
+
+        public IActionResult Register()
+        {
+            return View("Register");
+        }
+
+        [HttpPost]
+        public IActionResult SubmitLogIn(LogInViewModel model)
+        {
+            if(model.Equals(null) || string.IsNullOrEmpty(model.User.Username) || string.IsNullOrEmpty(model.User.Password))
+            {
+                model.ErrorMessage = "Username and password cannot be empty.";
+                return View("LogIn", model);
+            }
+            return View("LogIn", model);
+        }
+    }
+}
---
+++ MovieDirectoryDotNetCoreMVC/Models/LogInViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace MovieDirectoryDotNetCoreMVC.Models
+{
+    public class LogInViewModel
+    {
+        public User User { get; set; } = new User();
+        public string ErrorMessage { get; set; } = string.Empty;
+        public LogInViewModel() { }
+        public LogInViewModel(User user)
+        {
+            User = user;
+        }
+    }
+
+
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+}
